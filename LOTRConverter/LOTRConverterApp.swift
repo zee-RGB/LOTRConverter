22773a6397185e747e4fe44cf229ee66fdd61b07
zee-RGB <zaahir85@gmail.com>
2025-01-01 09:51:50 +1000

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by zee-RGB on 1/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
