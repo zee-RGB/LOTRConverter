fc177ea5df597d2245764e7699e5332616ce169b
zee-RGB <zaahir85@gmail.com>
2025-01-02 10:53:41 +1000

    updated ui features

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -8,14 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var infoScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            //Background Image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
+            
+            VStack() {
+                //Prancing Horse Image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
+
+                
+                //Currency Title
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                
+                HStack{
+                    //Left & right conversion
+                    VStack{
+                        HStack {
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                        }
+                        //TextField()
+                        Text("TextField()")
+                            .foregroundStyle(.white)
+                    }
+                    
+                    //Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
+                        .padding()
+                    
+                    
+                    VStack{
+                        HStack {
+                            Text("Gold penny")
+                                .font(.headline)
+                                .foregroundStyle(.white)
+                            Image(.goldpenny)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
+                        }
+                        //TextField()
+                        Text("TextField()")
+                            .foregroundStyle(.white)
+                    }
+                }
+                
+                
+                Spacer()
+                
+                HStack{
+                    Spacer()
+                    
+                    //Info Button
+                    Button {
+                        infoScreen.toggle()
+                        print(infoScreen)
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+
+                }
+                
+            }
+            .padding()
         }
-        .padding()
+        
     }
 }
 
